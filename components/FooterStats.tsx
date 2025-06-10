fba8d7af65a664d8f91977db198eb361125c5321
Shreyas <ishreyasr@gmail.com~>
2025-06-10 11:38:00 +0530

    Fix remaining ESLint errors in FooterStats

--- components/FooterStats.tsx
+++ components/FooterStats.tsx
@@ -11,20 +11,6 @@ interface FooterStatsProps {
     children: (stats: RepoStats) => ReactNode;
 }
 
-async function getRepoStats(): Promise<RepoStats> {
-    const repoStats = await fetch(
-        'https://api.github.com/repos/ShreyasR/portfolio-2.0',
-        {
-            next: {
-                revalidate: 60 * 60, // 1 hour
-            },
-        },
-    );
-
-    return repoStats.json();
-}
-
 export default function FooterStats({ children }: FooterStatsProps) {
-    const statsPromise = getRepoStats();
     return <>{children({ stargazers_count: 0, forks_count: 0 })}</>;
 } 
